50a673079af4ef37ae5fe2b3f8430a1829f80260
lbq <binqilanmail@163.cn>
2020-06-03 13:27:57 +0800

    spu查询

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # gmail0105测试
 gmall-user 用户服务：8080
 #测试分支
+用户service服务层8070
+web8080
--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/AttrController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/AttrController.java
@@ -2,9 +2,11 @@ package com.atguigu.gmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.PmsBaseAttrInfo;
+import com.atguigu.gmall.bean.PmsBaseAttrValue;
 import com.atguigu.gmall.service.AttrService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,5 +22,17 @@ public class AttrController {
     public List<PmsBaseAttrInfo> attrInfoList(String catalog3Id) {
         List<PmsBaseAttrInfo> pmsBaseAttrInfos = attrService.attrInfoList(catalog3Id);
         return pmsBaseAttrInfos;
+    }
+    @RequestMapping("saveAttrInfo")
+    @ResponseBody
+    public String saveAttrInfo(@RequestBody PmsBaseAttrInfo pmsBaseAttrInfo){
+        String success= attrService.saveAttrInfo(pmsBaseAttrInfo);
+        return "success";
+    }
+    @RequestMapping("getAttrValueList")
+    @ResponseBody
+    public List<PmsBaseAttrValue> getAttrValueList(String attrId){
+        List<PmsBaseAttrValue> pmsBaseAttrValues=attrService.getAttrValueList(attrId);
+        return pmsBaseAttrValues;
     }
 }
